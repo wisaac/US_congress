f44d7967d1f6e56f102fa7f43e69af631e053b0c
William Isaac <wsisaac@gmail.com>
2015-07-01 10:56:55 -0400

    Update 7/1-3
    Final Run of the Extraction

--- FDSYS_scrapper/fds.py
+++ FDSYS_scrapper/fds.py
@@ -10,7 +10,7 @@
 #Section 1: Module Import#
 ##########################
 
-import mechanize, sys, os, time, random, pprint, re
+import mechanize, sys, os, time, random, pprint, re, csv
 from BeautifulSoup import BeautifulSoup
 
 ###################################################
@@ -18,7 +18,7 @@ from BeautifulSoup import BeautifulSoup
 ###################################################
 
 new_path = "~/GitHub/US_congress/FDSYS_scrapper"
-outpath = os.path.expanduser(new_path)+'/output'
+outpath = os.path.expanduser(new_path)
 #os.chdir(new_path_1)
 #setting date and time
 nname = time.strftime("%b")
@@ -26,7 +26,7 @@ date = time.strftime("%d")
 ver = random.randrange(0, 999, 2)
 
 ####################################				
-# Section 3: Gather Core file links#
+#  Section 3: Writing Functions	   #
 ####################################
 
 def grab_links():
@@ -46,7 +46,6 @@ def grab_links():
 	for s_link in src_links:
 		response = br.open(s_link)
 		for link in br.links():
-		    print link.text, link.url
 		    if "More Information" in link.text:
 		            hr_links.append(link.url)
 	return hr_links
@@ -82,11 +81,13 @@ def grab_meta(hr_links):
 	 		print "This is the Text link:" + text_link
 	rawtext = br.open(text_link)
 	rawtext = rawtext.read()
+	rawtext = text_link
 
 	#Getting Hearing Date
 	tags = soup.findAll('td', text = re.compile("\w+\s\d+.\s\d+"), limit = 1)
 	date = str(tags)
 	print "This is the Date: " + date
+	date = date.strip("[ ]")
 	# for tag in tags:
 	# 		print tag.contents
 		  	# date = str(tag.next)
@@ -101,71 +102,37 @@ def grab_meta(hr_links):
 
 	#This can be used to extract raw text in a single line "bs4.BeautifulSoup(urllib.urlopen('http://google.com/?hl=en').read()).select('#footer a')"
 
-def write_file(csv_rows):
+def write_file(csv_rows = [], *args):
+	""" This function is to write out the files to csv """
 	
-
-
-
-
-	write_path = out_path + '/' + "Congressional_Record_Parsed"
+	#This is creating the output folder path and determining whether the directory exists.
+	write_path = outpath + '/' + "output"
 	ret = os.access(write_path, os.F_OK)
-	row = date, title, pdf_link, rawtext  
+	header = ('Date', 'Title', 'PDF link', 'Raw Text link')
+	z = len(csv_rows)
+
+	#Writing the CSV File
 	if ret == True:
 		os.chdir(write_path)
-		date_str = str(Date)+'.txt'
-		if os.access(date_str, os.F_OK) == True:
-			os.remove(date_str)
-		outputfile = open(date_str, 'wb+')
+		filename = 'tq_out.%s.%s.%03d.csv' %(nname,date,ver)
+		if os.access(filename, os.F_OK) == True:
+			os.remove(filename)
+		outputfile = open(filename, 'wb+')
+		writer = csv.writer(outputfile)
+		writer.writerow(header)
+		for i in range(z):
+			writer.writerow(csv_rows[i]) 
 		outputfile.close()
 	else:
-		os.makedirs(write_path, 0777 )
+		os.makedirs(write_path, 0777)
 		os.chdir(write_path)
-		date_str = str(Date)+'.txt'
-		outputfile = open(date_str, 'wb+')
-		outputfile_content = "This data:"+ str(Date)+ '\n' + "This has this many speakers:" + str(NumberofSpeakers) + '\n'+ "This file is the:" + str(count) + '\n' + str(SpeakerMatchObj2)
-		outputfile.write(outputfile_content) 
+		filename = 'tq_out.%s.%s.%03d.csv' %(nname,date,ver)
+		outputfile = open(filename, 'wb+')
+		writer = csv.writer(outputfile)
+		writer.writerow(header)
+		for i in range(z):
+			writer.writerow(csv_rows[i]) 
 		outputfile.close()
-
-
-try:
-    finished='N' 
-    #This is using the xml parsing software to pull the text from the file
-    os.chdir(inpath)
-    tree = ET.parse(xmltmp)
-    root=tree.getroot()
-    debate=[]
-    date = ""
-    member = ""
-    member_contribution = ""
-    title = ""
-    header = ('Date', 'Debate', 'Member', 'Member Contribution')
-    os.chdir(outpath)
-    csvfile=xmltmp+'.csv'
-    test= open(csvfile, 'wb+')
-    writer = csv.writer(test)
-    writer.writerow(header)
-    for node in tree.iter():
-      row = date, title, member, member_contribution      
-      #print node.tag, node.attrib, node.text
-      if node.tag == 'date':
-                  date = node.attrib
-      if node.tag == 'title':
-                  title = node.text.encode('ascii', 'ignore').decode('ascii')
-      if node.tag == 'member':
-                  member = node.text
-      if node.tag == 'membercontribution':
-                  member_contribution = node.text.encode('ascii', 'ignore').decode('ascii')
-                  print row
-                  writer.writerow(row)
-    test.close()
-    finished = 'Y'  
-  except finished == 'N':
-    raise RunError('Parser module did not finish')
-
-"def get pdfs"
-"def write csv"
-
-
 
 ####################################				
 # Section 4: Running Functions 	   #
@@ -183,53 +150,3 @@ for i in range(y):
 	row = date, title, pdf_link, rawtext
 	csv_rows.append(row)
 write_file(csv_rows)
-
-
-
-
-
-
-# Code Scrapyard
-################################################################################################################################################	
-'''
-
-pp = pprint.PrettyPrinter(indent=4)
-	pp_html = pp.pprint(html)
-	print pp_html
-
-	filename = 'htmlout.%s.%s.%01d.txt' %(nname,date,i)
-	output1 = open(filename, 'wb+')
-	print >> output1,test
-	output1.close()
-
-#These were useful modules from FDSYS github code
-#https://github.com/unitedstates/congress/blob/master/tasks/fdsys.py
-def get_package_files(package_name, granule_name, path):
-    baseurl = "http://www.gpo.gov/fdsys/pkg/%s" % package_name
-    baseurl2 = baseurl
-
-    if not granule_name:
-        file_name = package_name
-    else:
-        file_name = granule_name
-        baseurl2 = "http://www.gpo.gov/fdsys/granule/%s/%s" % (package_name, granule_name)
-
-    ret = {
-        # map file type names used on the command line to a tuple of the URL path on FDSys and the relative path on disk
-        'zip': (baseurl2 + ".zip", path + "/document.zip"),
-        'mods': (baseurl2 + "/mods.xml", path + "/mods.xml"),
-        'pdf': (baseurl + "/pdf/" + file_name + ".pdf", path + "/document.pdf"),
-        'xml': (baseurl + "/xml/" + file_name + ".xml", path + "/document.xml"),
-        'text': (baseurl + "/html/" + file_name + ".htm", path + "/document.html"),  # text wrapped in HTML
-    }
-    if not granule_name:
-        # granules don't have PREMIS files?
-        ret['premis'] = (baseurl + "/premis.xml", path + "/premis.xml")
-
-    return ret
-
-
-def unwrap_text_in_html(data):
-    text_content = unicode(html.fromstring(data).text_content())
-    return text_content.encode("utf8")
-'''
